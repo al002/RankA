9a745fe98ecd53d442f20451a0d433882f168e75
alayii <atsanmu@gmail.com>
2015-07-14 23:54:01 +0800

    request with Promise

--- index.js
+++ index.js
@@ -1,34 +1,49 @@
 var request = require('request');
 var cheerio = require('cheerio');
 
-var options = {
-  uri: 'https://torrentz.eu/search?q=' + process.argv[2],
-  proxy: 'http://127.0.0.1:1080',
-  headers: {
-      'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.132 Safari/537.36'
-  }
-}
+var url = 'https://torrentz.eu/search?q=' + process.argv[2];
 
-function callback(err, res, body) {
-  if (!err && res.statusCode === 200) {
-    var torrents = [];
-
-    $ = cheerio.load(body);
-
-    $('.results dl').each(function(index, elem) {
-      var link = $(elem).find('dt a').attr('href');
-      if (link !== undefined) {
-        torrents.push({
-            infoHash: link.substr(1),
-            keyword: process.argv[2],
-            name: $(elem).find('dt a').text(),
-            size: $(elem).find('dd .s').text()
-        });
+function requestp(url) {
+  return new Promise(function(resolve, reject) {
+    request({
+      uri: url,
+      proxy: 'http://127.0.0.1:1080',
+      headers: {
+          'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.132 Safari/537.36'
       }
-    });
+    }, function(err, res, body) {
+      if (err) {
+        return reject(err);
+      } else if (res.statusCode !== 200) {
+        err = new Error('Unexpected status code: ' + res.statusCode);
+        err.res = res;
 
-    return torrents;
-  }
+        return reject(err);
+      }
+
+      resolve(body);
+    })
+  });
 }
 
-request(options, callback);
+requestp(url).then(function(body) {
+  var torrents = [];
+
+  $ = cheerio.load(body);
+
+  $('.results dl').each(function(index, elem) {
+    var link = $(elem).find('dt a').attr('href');
+    if (link !== undefined) {
+      torrents.push({
+          infoHash: link.substr(1),
+          keyword: process.argv[2],
+          name: $(elem).find('dt a').text(),
+          size: $(elem).find('dd .s').text()
+      });
+    }
+  });
+
+  console.log(torrents);
+}).catch(function(err) {
+  console.log(err);
+});
