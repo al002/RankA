5f09c04be6cdc620e38f41950900e804c172ac3a
alayii <atsanmu@gmail.com>
2015-07-15 23:15:52 +0800

    single colon replace double colon

--- RankA/spiders/torrent_spider.py
+++ RankA/spiders/torrent_spider.py
@@ -1,12 +1,12 @@
 import scrapy;
 
 class TorrentSpider(scrapy.Spider):
-    name = "1337x"
-    allowed_domains = ["1337x.to"]
-    start_urls = ["http://1337x.to/sub/42/0/"]
+    name = '1337x'
+    allowed_domains = ['1337x.to']
+    start_urls = ['http://1337x.to/sub/42/0/']
 
 
     def parse(self, response):
-        for sel in response.xpath("//ul[@class='clearfix']/li"):
-            title = sel.xpath("div[@class='coll-1']/strong/a/text()").extract();
+        for sel in response.xpath('//ul[@class="clearfix"]/li'):
+            title = sel.xpath('div[@class="coll-1"]/strong/a/text()').extract();
             print(title)
